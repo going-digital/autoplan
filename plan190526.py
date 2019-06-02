17cf534c86c8f9dd29e82fc748668b2395aa3fd6
Peter Knight <cathedrow@icloud.com>
2019-06-02 20:56:09 +0100

    Refactor
    Restructure, generating report based on list of planning references. Add support for more than 25 documents per application.

---
+++ plan190526.py
@@ -0,0 +1,45 @@
+import tqdm
+import planning_portal
+import jinja2
+import datetime
+from pprint import pprint
+
+meeting_date = datetime.date(2019, 5, 28)
+reference_list = [
+    'DC/19/1966/FUL',
+    'DC/19/1848/FUL',
+    'DC/19/1951/FUL',
+    'DC/19/1950/FUL',
+    'DC/19/1906/FUL',
+]
+output_file = "test3.html"
+
+p = planning_portal.Planning()
+
+applications = {}
+    
+run_date = datetime.datetime.now()
+
+# Fetch metadata
+for reference in tqdm.tqdm(reference_list, desc='Metadata'):
+    applications[reference] = p.get_application(reference)
+
+# Fetch document links
+for reference in tqdm.tqdm(reference_list, desc='Doc Links'):
+    applications[reference]['documents'] = p.documents(reference)
+
+# Build report
+templateLoader = jinja2.FileSystemLoader(searchpath='./')
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = 'template.html.j2'
+template = templateEnv.get_template(TEMPLATE_FILE)
+pprint(applications)
+outputText = template.render(
+    title='Planning notes',
+    parish='Lowestoft',
+    run_date=run_date,
+    meeting_date=meeting_date,
+    applications=applications,
+)
+with open(output_file, 'w') as f:
+    f.write(outputText)
--- planning_portal.py
+++ planning_portal.py
@@ -1,7 +1,3 @@
-# Search -> Application
-# Application -> Metadata and documents
-# Document -> Metadata and contents
-
 import requests
 from requests.exceptions import RequestException
 from contextlib import closing
@@ -229,6 +225,52 @@ class Planning():
     def __init__(self):
          self.s = requests.Session()       
 
+    def get_application(self, reference):
+        # Fetch application metadata based on an application number
+        query = {
+            'searchType': 'Application',
+            'searchTypeStatus': 'All',
+            'searchCriteria.simpleSearchString': reference,
+            'searchCriteria.simpleSearch': 'true'
+        }        
+        url = BASE_URL + '/online-applications/simpleSearchResults.do?action=firstPage'
+        r1 = self.s.post(url, data=query, stream=True)
+        soup = BeautifulSoup(r1.content, 'html.parser')
+        results = {}
+        url = BASE_URL + soup.find(id='tab_summary')['href']
+        url_public_comments = BASE_URL + soup.find(id='tab_makeComment')['href']
+        url_constraints = BASE_URL + soup.find(id='tab_constraints')['href']
+        url_related = BASE_URL + soup.find(id='tab_relatedCases')['href']
+        url_documents = BASE_URL + soup.find(id='tab_externalDocuments')['href']
+        url_map = BASE_URL + soup.find(id='tab_map')['href']
+        address = soup.find('span', {'class': 'address'}).string.strip()
+        reference = soup.find('span', {'class': 'caseNumber'}).string.strip()
+        proposal = soup.find('span', {'class': 'description'}).string.strip()
+        received = datetime.datetime.strptime(
+            soup.find(id='simpleDetailsTable').find_all('tr')[1].td.string.strip(),
+            "%a %d %b %Y"
+        )
+        validated = datetime.datetime.strptime(
+            soup.find(id='simpleDetailsTable').find_all('tr')[2].td.string.strip(),
+            "%a %d %b %Y"
+        )
+        status = soup.find(id='simpleDetailsTable').find_all('tr')[5].td.span.string.strip()
+        results = {
+            'proposal': proposal,
+            'url': url,
+            'url_public_comments': url_public_comments,
+            'url_constraints': url_constraints,
+            'url_related': url_related,
+            'url_documents': url_documents,
+            'url_map': url_map,
+            'address': address,
+            'received': received,
+            'validated': validated,
+            'status': status,
+            'google_maps': "https://www.google.com/maps/search/?api=1&query=" + urllib.parse.quote(address)
+        }
+        return results
+
     def search(self, start_date, end_date, parish='', ward=''):
         "Search planning portal for relevant applications"
         query = {
@@ -369,7 +411,7 @@ if __name__ == "__main__":
             end_date=datetime.date(2019, 5, 16)
         )
         pprint(applications)
-    if True:
+    if False:
         # The application below has more than 25 documents,
         # and requires multiple index page fetches.
         documents = p.documents('DC/19/1906/FUL')
@@ -377,7 +419,9 @@ if __name__ == "__main__":
     if False:
         document = p.get_document('http://publicaccessdocuments.eastsuffolk.gov.uk/AniteIM.WebSearch/Download.aspx?ID=1480471')
         pprint(document)
-
+    if True:
+        pprint(p.get_application('DC/19/1906/FUL'))
+        pprint(p.documents('DC/19/1906/FUL'))
     if False:
         applications = p.search(
             Parish.LOWESTOFT,
@@ -391,8 +435,6 @@ if __name__ == "__main__":
             os.mkdir(dir_name)
             documents = p.documents(ref)
             for i in documents:
-                print(i)
-                print(i['description'])
                 document = p.get_document(i['url'])
                 filename = os.path.join(dir_name, document['filename'])
                 with open(filename, 'wb') as f:
